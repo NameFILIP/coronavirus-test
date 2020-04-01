9fe02b7db26fd1b37b5ac3f3295f498bf06889cc
Filip Spiridonov <fspiridonov@uber.com>
2020-04-01 16:39:44 -0700

    Build project into docs folder

---
+++ docs/precache-manifest.4882f9e13e43df2eaf293403ac611a2a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f1b3033749c329a072639cc00f3ad5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635853252a86b704920b",
+    "url": "/static/js/2.a28f8010.chunk.js"
+  },
+  {
+    "revision": "6a89b3c0a9fb18783279",
+    "url": "/static/js/main.fa2b49b9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
