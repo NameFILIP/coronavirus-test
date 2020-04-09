9d5ec83f5af2b021f15b13aae5f0eeebe5fa3ee8
Filip Spiridonov <fspiridonov@uber.com>
2020-04-09 09:36:49 -0700

    Add homepage to have relative paths

---
+++ docs/precache-manifest.1206aa5636cd10e8c348edbb9930650a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eefbd7b6cbc59eb2588c9636d4f7dbb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "635853252a86b704920b",
+    "url": "./static/js/2.a28f8010.chunk.js"
+  },
+  {
+    "revision": "377812478a04f2bb9a44",
+    "url": "./static/js/main.16423358.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35445fe0b54dc724712f51d6fcfa72c9.js"
+  "./precache-manifest.1206aa5636cd10e8c348edbb9930650a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
